a1efe41a83010bf5269bf1e03d234a0402bd2341
Usman Yousaf <officialusmanyousaf@gmail.com>
2023-11-13 12:40:48 +0500

    updated home

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1>Hello World -Nextjs14</h1>
+      <h2>Second Line is added to test auto deployment of vercel on Nextjs apps.</h2>
     </main>
   )
 }
